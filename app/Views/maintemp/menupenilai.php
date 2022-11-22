0c5e39f3dfc4a2d84154b2c1c4e00855bda53a27
gdebig <gdebig@gmail.com>
2022-11-23 06:25:56 +0700

    Update penilai

--- app/Views/maintemp/menupenilai.php
+++ app/Views/maintemp/menupenilai.php
@@ -5,8 +5,8 @@
     </a>
 </li>
 <li class="nav-item">
-    <a href="<?php echo base_url();?>/mantugasakhir" class="nav-link">
+    <a href="<?php echo base_url();?>/pengujian" class="nav-link">
         <i class="fas fa-graduation-cap nav-icon"></i>
-        <p>Penilaian Tugas Akhir</p>
+        <p>Pengujian Praktek Keinsinyuran</p>
     </a>
 </li>
