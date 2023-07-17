cb58c99fdbc51957355f50ed5bfa30fe09c3e87a
gdebig <gdebig@gmail.com>
2023-07-18 01:03:52 +0700

    update apifaip

--- app/Controllers/Apifaip.php
+++ app/Controllers/Apifaip.php
@@ -59,7 +59,7 @@ class Apifaip extends BaseController
                 #insert FAIP 1.1.1
                 $model111 = new ProfileModel();
                 $user = $model111->where('tbl_profile.user_id', $id)->join('tbl_user', 'tbl_profile.user_id = tbl_user.user_id', 'left')->first();
-                $data = "user_id=\"16906\"&addr_type=1&addr_desc='" . $user['HAddr'] . "'&addr_loc='" . $user['HCity'] . "'&addr_zip='" . $user['HPostnum'] . "'";
+                $data = "user_id=\"26906\"&addr_type=1&addr_desc='" . $user['HAddr'] . "'&addr_loc='" . $user['HCity'] . "'&addr_zip='" . $user['HPostnum'] . "'";
                 $insertfaip111 = $sendfaip->sendfaip111($token, $data);
                 if ($insertfaip111 != "ok") {
                     $session->setFlashdata('err', "Insert FAIP 1.1.1. gagal.");
@@ -67,9 +67,8 @@ class Apifaip extends BaseController
                 }
 
                 #insert FAIP 1.1.2
-                $data = "user_id=\"16906\"&exp_name='" . $user['Position'] . "'&exp_loc='" . $user['WAddr'] . ", " . $user['WCity'] . "'&exp_zip='" . $user['Wpostnum'] . "'";
+                $data = "user_id=\"26906\"&exp_name='" . $user['Position'] . "'&exp_loc='" . $user['WAddr'] . ", " . $user['WCity'] . "'&exp_zip='" . $user['Wpostnum'] . "'";
                 $insertfaip112 = $sendfaip->sendfaip112($token, $data);
-                echo $insertfaip112;
                 if ($insertfaip112 != "ok") {
                     $session->setFlashdata('err', "Insert FAIP 1.1.2. gagal.");
                     return redirect()->to('/apifaip');
